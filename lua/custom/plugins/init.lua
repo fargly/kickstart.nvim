166204b11efb6ad2715f6b8e58be9d0fb5472504
fargly <kurt.danielson@gmail.com>
2024-06-06 17:18:48 +0000

    Config Attempt/FAIL

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,7 +6,7 @@ return {
   'epwalsh/obsidian.nvim',
   version = '*', -- recommended, use latest release instead of latest commit
   conceallevel = 1,
-  lazy = true,
+  lazy = false,
   ft = 'markdown',
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
