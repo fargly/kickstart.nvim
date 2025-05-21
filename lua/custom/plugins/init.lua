139413b5699bbb6933aa84cf1e06ea1da985539a
fargly <kurt.danielson@gmail.com>
2025-05-21 08:09:44 -0500

    Add markdown conceal

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,18 +8,8 @@ return {
     version = '*', -- recommended, use latest release instead of latest commit
     lazy = false,
     ft = 'markdown',
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-    --   "BufReadPre path/to/my-vault/**.md",
-    --   "BufNewFile path/to/my-vault/**.md",
-    -- },
     dependencies = {
-      -- Required.
       'nvim-lua/plenary.nvim',
-
-      -- see below for full list of optional dependencies 👇
     },
     opts = {
       workspaces = {
@@ -27,16 +17,20 @@ return {
           name = 'farglyNotes',
           path = '~/Projects/Fargly/fNv5',
         },
-        -- {
-        --   name = 'work',
-        --   path = '~/vaults/work',
-        -- },
       },
-
-      -- see below for full list of options 👇
     },
-    -- vim.opt.conceallevel = 2
-    vim.keymap.set('n', '<C-l>', '<C-w>w', { desc = 'Switch Vim Panes' }),
+    config = function(_, opts)
+      require('obsidian').setup(opts)
+      -- Set conceallevel = 2 only for markdown buffers
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'markdown',
+        callback = function()
+          vim.opt_local.conceallevel = 1
+        end,
+      })
+      -- Keymap for switching panes
+      vim.keymap.set('n', '<C-l>', '<C-w>w', { desc = 'Switch Vim Panes', buffer = true })
+    end,
   },
   {
     'MeanderingProgrammer/render-markdown.nvim',
